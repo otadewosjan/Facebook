b232ae6169dcfd0e264f40ab083b67705302389f
otadewosjan <oriana.an@outlook.com>
2018-08-10 15:38:50 +0200

    changed to jQuery

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function loadJsonDoc() {
-    let myDoc;
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://jsonplaceholder.typicode.com/posts", true);
-
-    xhr.onprogress = function () {
-        console.log('LOADING...', xhr.readyState);
-    };  
-
-    xhr.onload = function() {
-        if (this.status === 200) {
-            myDoc = JSON.parse(this.responseText);
-
-            let outputList = "";
-            for(let i in myDoc) {
-                outputList += `<li> ${myDoc[i].title} <hr> ${myDoc[i].body} </li><br><br>`;
-            }
-            document.getElementById("postList").innerHTML = outputList;
-
+$(document).ready(function () {
+    $.getJSON("http://jsonplaceholder.typicode.com/posts", function (facebookposts) {
+        for (let i in facebookposts) {
+            $("#postList").append(`<li> ${facebookposts[i].title} <hr> ${facebookposts[i].body} </li><br><br>`)
         }
-    };
-
-    xhr.send();
-};
+    });
+});
