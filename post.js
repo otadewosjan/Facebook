85cb54377d2286910ed5623bc23134b1c80da610
otadewosjan <oriana.an@outlook.com>
2018-08-22 09:19:31 +0200

    moved infinie scroll to Post class

--- post.js
+++ post.js
@@ -1,7 +1,7 @@
 export class Post {
 
     get() {
-        $.getJSON("http://jsonplaceholder.typicode.com/posts", (posts) => {this.render([posts[0],posts[1]])})
+        $.getJSON("http://jsonplaceholder.typicode.com/posts", (posts) => { this.render([posts[0], posts[1]]) })
     }
 
     render(posts) {
@@ -10,6 +10,23 @@ export class Post {
             $(posts).each(function (i, value) {
                 $("#posts").append(compiledTemplate(value));
             });
+            this.infiniteScroll();
+        });
+    }
+
+    infiniteScroll() {
+
+        let win = $(window);
+
+        function isScrollbarAtBottom() {
+            return ($(document).height() - win.height() === Math.floor(win.scrollTop()) ||
+                ($(document).height() - win.height() === Math.floor(win.scrollTop()) + 1));
+        }
+
+        win.scroll(() => {
+            if (isScrollbarAtBottom()) {
+                this.get();
+            }
         });
     }
 }
--- script.js
+++ script.js
@@ -2,20 +2,3 @@ import { Post } from '/post.js';
 
 let posts = new Post();
 posts.get();
-infiniteScroll(posts);
-
-function infiniteScroll(obj) {
-
-    let win = $(window);
-
-    function isScrollbarAtBottom() {
-        return ($(document).height() - win.height() === Math.floor(win.scrollTop()) ||
-            ($(document).height() - win.height() === Math.floor(win.scrollTop()) + 1));
-    }
-
-    win.scroll(function () {
-        if (isScrollbarAtBottom()) {
-            obj.get();
-        }
-    });
-}
